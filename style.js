d81335538e865b29f5295152391bc92353716c2c
KeanuGreavez <wahidur95@hotmail.com>
2026-03-13 21:23:28 +0000

    Update style.js

--- style.js
+++ style.js
@@ -1,14 +1,33 @@
-function getPrayerTimesByCity() {
-fetch("https://api.aladhan.com/v1/timingsByCity?city=Middleton&country=UK&method=2")
-.then(response => response.json())
-.then(data => {
-    let t = data.data.timings;
-    document.getElementById("fajr").innerText = t.Fajr;
-    document.getElementById("dhuhr").innerText = t.Dhuhr;
-    document.getElementById("asr").innerText = t.Asr;
-    document.getElementById("maghrib").innerText = t.Maghrib;
-    document.getElementById("isha").innerText = t.Isha;
-});
-}
+// Ask user to click a button first to allow location (required on some phones)
+function requestPrayerTimes() {
+    if (!navigator.geolocation) {
+        alert("Geolocation is not supported by your browser");
+        return;
+    }
 
-getPrayerTimesByCity();
+    navigator.geolocation.getCurrentPosition(
+        (position) => {
+            let lat = position.coords.latitude;
+            let lon = position.coords.longitude;
+
+            fetch(`https://api.aladhan.com/v1/timings?latitude=${lat}&longitude=${lon}&method=2`)
+            .then(response => response.json())
+            .then(data => {
+                let t = data.data.timings;
+
+                document.getElementById("fajr").innerText = t.Fajr;
+                document.getElementById("dhuhr").innerText = t.Dhuhr;
+                document.getElementById("asr").innerText = t.Asr;
+                document.getElementById("maghrib").innerText = t.Maghrib;
+                document.getElementById("isha").innerText = t.Isha;
+            })
+            .catch(err => {
+                console.error(err);
+                alert("Failed to fetch prayer times");
+            });
+        },
+        (err) => {
+            alert("Please allow location access to see prayer times");
+        }
+    );
+}
