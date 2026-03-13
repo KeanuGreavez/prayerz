4896c1f37078cc610cec8e819d1d773c5e885c9e
KeanuGreavez <wahidur95@hotmail.com>
2026-03-13 21:22:22 +0000

    Update style.js

--- style.js
+++ style.js
@@ -1,23 +1,14 @@
-function getPrayerTimes(lat, lon){
-
-fetch(`https://api.aladhan.com/v1/timings?latitude=${lat}&longitude=${lon}&method=2`)
-.then(res => res.json())
+function getPrayerTimesByCity() {
+fetch("https://api.aladhan.com/v1/timingsByCity?city=Middleton&country=UK&method=2")
+.then(response => response.json())
 .then(data => {
-
-let t = data.data.timings;
-
-document.getElementById("fajr").innerText = t.Fajr;
-document.getElementById("dhuhr").innerText = t.Dhuhr;
-document.getElementById("asr").innerText = t.Asr;
-document.getElementById("maghrib").innerText = t.Maghrib;
-document.getElementById("isha").innerText = t.Isha;
-
+    let t = data.data.timings;
+    document.getElementById("fajr").innerText = t.Fajr;
+    document.getElementById("dhuhr").innerText = t.Dhuhr;
+    document.getElementById("asr").innerText = t.Asr;
+    document.getElementById("maghrib").innerText = t.Maghrib;
+    document.getElementById("isha").innerText = t.Isha;
 });
-
 }
 
-navigator.geolocation.getCurrentPosition(pos => {
-
-getPrayerTimes(pos.coords.latitude, pos.coords.longitude);
-
-});
+getPrayerTimesByCity();
