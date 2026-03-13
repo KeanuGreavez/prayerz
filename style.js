3c756da08b1c6c3211921bcc194039bb82fc2500
KeanuGreavez <wahidur95@hotmail.com>
2026-03-13 20:23:14 +0000

    Update style.js

--- style.js
+++ style.js
@@ -1,1 +1,19 @@
+let prayerTimes = {};
 
+async function loadTimes() {
+
+let response = await fetch("https://api.aladhan.com/v1/timingsByCity?city=London&country=UK");
+let data = await response.json();
+
+prayerTimes = data.data.timings;
+
+}
+
+function showTime(prayer){
+
+document.getElementById("result").innerText =
+prayer + " time: " + prayerTimes[prayer];
+
+}
+
+loadTimes();
